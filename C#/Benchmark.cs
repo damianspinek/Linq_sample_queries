03236e5cee1d6a423500e686477c8bc556859d7b
Damian Spinek <damian00737@gmail.com>
2016-11-15 17:31:02 +0100

    lab 2 and 3

---
+++ C#/Benchmark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace QuerySamples
+{
+    class Benchmark
+    {
+       public static double getTimeOf(Func<IEnumerable<object>> func)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+
+            func.Invoke().Count();
+
+            watch.Stop();
+            return watch.Elapsed.TotalMilliseconds;
+        }
+    }
+}
